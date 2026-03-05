4bb019d020eb3b3903e7c49143f0755c36b34d89
Thailan Gouvêa de Menezes <thailangm@icloud.com>
2026-03-05 09:51:37 +0200

    applies selection sort algorithm

---
+++ selection.py
@@ -0,0 +1,22 @@
+def selection_sort(lista):
+    new_lista = lista
+
+    for i in range(len(new_lista)):
+        min_index = i
+        count = 0
+
+        for j in range(i + 1, len(new_lista)):
+            count += 1
+            if(new_lista[j] < new_lista[min_index]):
+                min_index = j
+
+        temp = new_lista[i]
+        new_lista[i] = new_lista[min_index]
+        new_lista[min_index] = temp
+
+    return new_lista
+
+lista = [3, 2, 1, 4, 5]
+new_lista = selection_sort(lista)
+print(new_lista)
+
